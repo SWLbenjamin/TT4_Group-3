d759ffd888b90f4d0131abd64f8584b36f78ef54
Cheng Yen <e0136987@u.nus.edu>
2022-03-25 12:02:49 +0800

    add transaction api

---
+++ backend/TransactionRouter.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+
+class TransactionRouter {
+
+    constructor(app, db) {
+        this.getBalance(app, db);
+        this.getLoan(app, db);
+        this.createLoan(app, db);
+        this.updatePaymentAndLoan(app, db);
+    }
+
+    getBalance(app, db) {
+        app.get('/', (req, res) => {
+          customerID = req.params.id;
+          db.query("SELECT balance FROM customers WHERE CustomerId =" + customerID, function (err, result, fields) {
+            if (err) throw err;
+            res.send(result);
+        })
+      }
+
+    getLoan(app, db) {
+        app.get('/', (req, res) => {}   )
+      }
+
+    createLoan(app, db) {
+        app.post('/', (req, res) => {}   )
+      }
+
+    updatePaymentAndLoan(app, db) {
+        app.post('/', (req, res) => {}   )
+      }
+}
+
+
+module.exports = TransactionRouter;
