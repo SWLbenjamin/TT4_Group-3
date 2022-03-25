6772af65a9c79cdf8a6e0b4ac3e1f6eecae13a65
SWLBenjamin <SWLBenjamin@gmail.com>
2022-03-25 13:56:33 +0800

    Debugged API, rerouted DB address

--- backend/TransactionRouter.js
+++ backend/TransactionRouter.js
@@ -5,65 +5,66 @@ class TransactionRouter {
     constructor(app, db) {
         this.getBalance(app, db);
         this.getLoan(app, db);
-        this.createLoan(app, db);
-        this.updatePaymentAndLoan(app, db);
+        // this.createLoan(app, db);
+        // this.updatePaymentAndLoan(app, db);
     }
 
     getBalance(app, db) {
-        app.get('/:id', (req, res) => {
-          customerID = req.params.id;
-          db.query("SELECT balance FROM customer WHERE CustomerId =" + customerID, function (err, result, fields) {
-            if (err) throw err;
-            res.send(result);
-        });
-      }
-    );
-      }
+        app.get('/getBalance', (req, res) => {
+            let customerID = req.body.id;
+            db.query("SELECT balance FROM customer WHERE CustomerId =" + customerID, function (err, result, fields) {
+                if (err) throw err;
+                res.send(result);
+            });
+        }
+        );
+    }
 
 
 
     getLoan(app, db) {
         app.get('/getLoan', (req, res) => {
-          db.query("SELECT SUM(loan_amount) FROM loan WHERE LoanId IN (SELECT LoanId FROM customerloan WHERE CustomerId=" + customerID +")" , function (err, result, fields) {
-            if (err) throw err;
-            res.send(result);
-        });
-      }
-      );
+            let customerID = req.body.id;
+            db.query("SELECT SUM(loan_amount) FROM loan WHERE LoanId IN (SELECT LoanId FROM customerloan WHERE CustomerId=" + customerID + ")", function (err, result, fields) {
+                if (err) throw err;
+                res.send(result);
+            });
+        }
+        );
     }
 
-    createLoan(app, db) {
-        app.post('/', (req, res) => {
-          data = {loan_amount = req.body.loan, customerId = req.body.id};
-          let loanID = 0;
-          db.query(`INSERT INTO loan (loan_amount) VALUES ('${data.loan_amount}')`, function (err,results){
-            if (err) throw error;
-            res.send('Successfully added loan!');
-          });
-          db.query('SELECT LAST_INSERT_ID()', function (err,results){
-            if (err) throw error;
-            loanID = results;
-          });
-          db.query(`INSERT INTO customerloan (CustomerId, LoanId) VALUES ('${data.customerID}','${loanID}')`, function (err,results){
-            if (err) throw error;
-            res.send('Successfully added loan!');
-          });
-        }   )
-      }
+    // createLoan(app, db) {
+    //     app.post('/', (req, res) => {
+    //       data = {loan_amount = req.body.loan, customerId = req.body.id};
+    //       let loanID = 0;
+    //       db.query(`INSERT INTO loan (loan_amount) VALUES ('${data.loan_amount}')`, function (err,results){
+    //         if (err) throw error;
+    //         res.send('Successfully added loan!');
+    //       });
+    //       db.query('SELECT LAST_INSERT_ID()', function (err,results){
+    //         if (err) throw error;
+    //         loanID = results;
+    //       });
+    //       db.query(`INSERT INTO customerloan (CustomerId, LoanId) VALUES ('${data.customerID}','${loanID}')`, function (err,results){
+    //         if (err) throw error;
+    //         res.send('Successfully added loan!');
+    //       });
+    //     }   )
+    //   }
 
-    updatePaymentAndLoan(app, db) {
-        app.post('/updatePaymentAndLoan', (req, res) => {
-          data = {payment_amount = req.body.payment, loanId = req.body.id};
-          db.query(`UPDATE loan SET loan_amount = loan_amount - ('${data.payment_amount}') WHERE loanId = ('${data.loanId}')`, function (err,results){
-            if (err) throw error;
-            res.send('Successfully paid!');
-          });
-          db.query(`INSERT INTO payment (LoanId, payment_date, payment_amount) VALUES ('${data.loanId}','2022-03-25','${data.payment_amount}')`, function (err,results){
-            if (err) throw error;
-            res.send('Successfully paid!');
-          });
-        }  );
-      }
+    // updatePaymentAndLoan(app, db) {
+    //     app.post('/updatePaymentAndLoan', (req, res) => {
+    //       data = {payment_amount = req.body.payment, loanId = req.body.id};
+    //       db.query(`UPDATE loan SET loan_amount = loan_amount - ('${data.payment_amount}') WHERE loanId = ('${data.loanId}')`, function (err,results){
+    //         if (err) throw error;
+    //         res.send('Successfully paid!');
+    //       });
+    //       db.query(`INSERT INTO payment (LoanId, payment_date, payment_amount) VALUES ('${data.loanId}','2022-03-25','${data.payment_amount}')`, function (err,results){
+    //         if (err) throw error;
+    //         res.send('Successfully paid!');
+    //       });
+    //     }  );
+    //   }
 }
 
 
